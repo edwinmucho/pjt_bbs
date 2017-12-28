a40be5dadf0dd4643cc10322082146726fd914ac
Edwin <edwin.mucho@gmail.com>
2017-12-29 01:12:06 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+###python version : 3.6.4
+###Django version : 2.0
---
+++ bbs/views.py
@@ -0,0 +1,40 @@
+from django.shortcuts import render
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from bbs.models import Bbs
+from bbs.serializers import BbsSerializer
+
+
+@api_view(['GET', 'POST'])
+def bbs_list(request, format=None):
+    if request.method == 'GET':
+        bbs = Bbs.objects.all()
+        serializer = BbsSerializer(bbs, many=True) # many 값이 True 이면 다수의 Data instance를 직렬화할 수 있다.
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = BbsSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET','POST','DELETE'])
+def bbs_detail(request, pk, format=None):
+    try:
+        bbs = Bbs.objects.get(pk=pk)
+    except Bbs.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == "GET":
+        serializer = BbsSerializer(bbs)
+        return Response(serializer.data)
+    elif request.method == "POST":
+        serializer = BbsSerializer(bbs, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'DELETE':
+        bbs.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
