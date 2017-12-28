f7f6e2d3df5e12661682f6688c9b09e0a97e6274
Edwin <edwin.mucho@gmail.com>
2017-12-29 01:19:16 +0900

    modified class based

--- bbs/urls.py
+++ bbs/urls.py
@@ -3,8 +3,13 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from bbs import views
 
 urlpatterns = [
-    path('', views.bbs_list),
-    path('<int:pk>/bbs', views.bbs_detail),
+    # Class 기반 View 처리
+    path('', views.BbsList.as_view()),
+    path('<int:pk>/bbs', views.BbsDetail.as_view()),
+
+    # 함수 기반 View 처리
+    # path('', views.bbs_list),
+    # path('<int:pk>/bbs', views.bbs_detail),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
